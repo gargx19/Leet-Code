f761ef81965246fdad72232015e0a51a68493e53
KRISH GARG <krishgarg164@gmail.com>
2026-08-12 21:58:51 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.86%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        nums.push_back(target);
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                return i;
+                break;
+            }
+        }
+        return -1;
+    }
+};
